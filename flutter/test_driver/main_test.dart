229637ff98887614314200e682a1c053213790f8
Artem Sheremet <dot.doom@gmail.com>
2020-09-18 18:03:47 +0200

    Make test startup stable (wait for 1st frame)

--- flutter/test_driver/main_test.dart
+++ flutter/test_driver/main_test.dart
@@ -77,6 +77,7 @@ void main() {
     setUpAll(() async {
       driver = await FlutterDriver.connect();
       localizations = const AppLocalizations();
+      await driver.waitUntilFirstFrameRasterized();
     });
 
     tearDownAll(() async {
