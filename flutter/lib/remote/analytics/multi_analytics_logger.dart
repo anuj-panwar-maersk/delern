bac853a5bb73e731d03e9f440e84e5763165b60b
Katarina Sheremet <kate.romanowskaya@gmail.com>
2020-10-14 09:47:07 +0200

    Refactor multi analytics

--- flutter/lib/remote/analytics/multi_analytics_logger.dart
+++ flutter/lib/remote/analytics/multi_analytics_logger.dart
@@ -8,21 +8,27 @@ class MultiAnalyticsLogger extends AnalyticsProvider {
 
   @override
   Future<void> logEvent({String name, Map<String, dynamic> parameters}) async {
-    analyticList.forEach((analytic) async {
+    // https://github.com/dart-lang/linter/issues/1099
+    // ignore: avoid_types_on_closure_parameters
+    await Future.forEach(analyticList, (AnalyticsProvider analytic) async {
       await analytic.logEvent(name: name, parameters: parameters);
     });
   }
 
   @override
   Future<void> setCurrentScreen({String screenName}) async {
-    analyticList.forEach((analytic) async {
+    // https://github.com/dart-lang/linter/issues/1099
+    // ignore: avoid_types_on_closure_parameters
+    await Future.forEach(analyticList, (AnalyticsProvider analytic) async {
       await analytic.setCurrentScreen(screenName: screenName);
     });
   }
 
   @override
   Future<void> setUserId(String id) async {
-    analyticList.forEach((analytic) async {
+    // https://github.com/dart-lang/linter/issues/1099
+    // ignore: avoid_types_on_closure_parameters
+    await Future.forEach(analyticList, (AnalyticsProvider analytic) async {
       await analytic.setUserId(id);
     });
   }
